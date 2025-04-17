722568a070f997a106902df8db59ed4471c6034f
Davide Berweger <davide.berweger@zubi.swiss>
2025-04-17 23:23:23 +0200

    Update server configuration to run on port 8080 instead of 6000.

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ def add_todo():
     return redirect(url_for('start'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=6000)
+    app.run(debug=True, host='0.0.0.0', port=8080)
